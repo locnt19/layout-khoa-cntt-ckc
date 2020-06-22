b765bafed06107514240433d1dc5f2b50a4caf65
Loc Thanh <locnt19@gmail.com>
2020-06-23 03:09:43 +0700

    highlight_home

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   LazyLoadFunction();
+  allSliderFuntion();
+  allEventClickFunction();
 });
 
 function LazyLoadFunction() {
@@ -7,4 +9,48 @@ function LazyLoadFunction() {
     elements_selector: '.lazy',
   });
   lazyLoad.update();
+}
+
+function allEventClickFunction() {
+  $('.highlight_home__slider__thumbnail .highlight_home__slider__item').click(function () {
+    $('.highlight_home__slider__item').removeClass('active');
+    $(this).addClass('active');
+    // console.log($(this).children('.highlight_home__subtitle'));
+    const title = $(this).children('.highlight_home__sub_title').text();
+    const short = $(this).children('.highlight_home__sub_desc').text();
+    const postTime = $(this).children('.highlight_home__sub_postedtime').text();
+    $('.highlight_home__title').text(title);
+    $('.highlight_home__desc').text(short);
+    $('.highlight_home__postedtime').text(postTime);
+  })
+}
+
+function allSliderFuntion() {
+  var galleryThumbs = new Swiper('.highlight_home__slider__thumbnail', {
+    slidesPerView: 3,
+    speed: 2000,
+    breakpoints: {},
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true
+  });
+  new Swiper('.highlight_home__slider', {
+    spaceBetween: 15,
+    speed: 2500,
+    slideToClickedSlide: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    },
+  });
 }
