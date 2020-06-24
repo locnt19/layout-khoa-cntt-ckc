30fa69acffebc1801a13910a8b802d9f7157993d
Loc Thanh <locnt19@gmail.com>
2020-06-24 15:24:43 +0700

    index page ➡ done

--- src/components/main.js
+++ src/components/main.js
@@ -16,6 +16,9 @@ function LazyLoadFunction() {
 }
 
 function eventClickHighLightHome() {
+  $('.highlight_home__slider__thumbnail .swiper-slide').on('change', function () {
+    $(this)
+  })
   $('.highlight_home__slider__thumbnail .highlight_home__slider__item').click(function () {
     $('.highlight_home__slider__item').removeClass('active')
     $(this).addClass('active')
@@ -29,10 +32,14 @@ function eventClickHighLightHome() {
 }
 
 function sliderHighLightHome() {
-  var galleryThumbs = new Swiper('.highlight_home__slider__thumbnail', {
-    slidesPerView: 3,
-    speed: 2000,
-    breakpoints: {},
+  var highLightHomeThumbs = new Swiper('.highlight_home__slider__thumbnail', {
+    speed: 800,
+    breakpoints: {
+      slidesPerView: 1,
+      768: {
+        slidesPerView: 3
+      }
+    },
     scrollbar: {
       el: '.swiper-scrollbar',
       hide: true,
@@ -42,29 +49,48 @@ function sliderHighLightHome() {
   })
   new Swiper('.highlight_home__slider', {
     spaceBetween: 15,
-    speed: 2500,
     slideToClickedSlide: true,
-    simulateTouch: false,
     effect: 'coverflow',
     coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
+      rotate: 90,
+      stretch: 90,
       depth: 100,
       modifier: 1,
       slideShadows: true,
     },
+    lazy: true,
+    speed: 2000,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    simulateTouch: false, // tắt kéo-thả chuột khi chuyển slide
+    breakpoints: {
+      992: {
+        autoplay: false,
+      }
+    },
+    pagination: {
+      el: '.highlight_home__slider .swiper-pagination',
+      clickable: true
+    },
     thumbs: {
-      swiper: galleryThumbs
+      swiper: highLightHomeThumbs
     },
   })
 }
 
 function sliderJobHome() {
   new Swiper('.job_home__slider', {
-    slidesPerView: 1,
+    // autoHeight: true,
+    lazy: true,
     effect: 'fade',
     fadeEffect: {
       crossFade: true
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
     },
     loop: true,
     speed: 1000,
@@ -80,8 +106,9 @@ function sliderGalleryHome() {
     spaceBetween: 30,
     loop: true,
     centeredSlides: true,
+    lazy: true,
     autoplay: {
-      delay: 5000,
+      delay: 4000,
       disableOnInteraction: false,
     },
     speed: 2000,
@@ -120,18 +147,19 @@ function sliderPartnerHome() {
 }
 
 function sliderNewsHome() {
-  new Swiper('.home-news__main .swiper-container', {
+  new Swiper('.news_home__main .swiper-container', {
     slideToClickedSlide: true,
     effect: 'fade',
     fadeEffect: {
       crossFade: true
     },
+    lazy: true,
     spaceBetween: 16,
     loop: true,
-    speed: 1500,
+    speed: 1000,
     navigation: {
-      nextEl: '.home-news__main__nav-next',
-      prevEl: '.home-news__main__nav-prev'
+      nextEl: '.news_home__main__nav-next',
+      prevEl: '.news_home__main__nav-prev'
     },
     autoplay: {
       delay: 5000,
@@ -143,14 +171,14 @@ function sliderNewsHome() {
       }
     }
   })
-  new Swiper('.home-news__sub .swiper-container', {
+  new Swiper('.news_home__sub .swiper-container', {
     direction: 'vertical',
     slidesPerView: 'auto',
     autoHeight: true,
     loop: true,
     navigation: {
-      nextEl: '.home-news__sub__nav-next',
-      prevEl: '.home-news__sub__nav-prev'
+      nextEl: '.news_home__sub__nav-next',
+      prevEl: '.news_home__sub__nav-prev'
     },
   })
 }
