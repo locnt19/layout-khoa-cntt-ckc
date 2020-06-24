31b7e9480765baf6dbac63cbe7212553e7dbf5c4
Loc Thanh <locnt19@gmail.com>
2020-06-24 08:50:17 +0700

    commit for megre

--- src/components/main.js
+++ src/components/main.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
-  LazyLoadFunction();
-  sliderHighLightHome();
-  eventClickHighLightHome();
-  sliderJobHome();
-  sliderGalleryHome();
-  sliderParnerHome();
-});
+  LazyLoadFunction()
+  sliderHighLightHome()
+  eventClickHighLightHome()
+  sliderJobHome()
+  sliderGalleryHome()
+  sliderPartnerHome()
+  sliderNewsHome()
+})
 
 function LazyLoadFunction() {
   var lazyLoad = new LazyLoad({
     elements_selector: '.lazy',
-  });
-  lazyLoad.update();
+  })
+  lazyLoad.update()
 }
 
 function eventClickHighLightHome() {
   $('.highlight_home__slider__thumbnail .highlight_home__slider__item').click(function () {
-    $('.highlight_home__slider__item').removeClass('active');
-    $(this).addClass('active');
-    const title = $(this).children('.highlight_home__sub_title').text();
-    const short = $(this).children('.highlight_home__sub_desc').text();
-    const postTime = $(this).children('.highlight_home__sub_postedtime').text();
-    $('.highlight_home__title').text(title);
-    $('.highlight_home__desc').text(short);
-    $('.highlight_home__postedtime').text(postTime);
+    $('.highlight_home__slider__item').removeClass('active')
+    $(this).addClass('active')
+    const title = $(this).children('.highlight_home__sub_title').text()
+    const short = $(this).children('.highlight_home__sub_desc').text()
+    const postTime = $(this).children('.highlight_home__sub_postedtime').text()
+    $('.highlight_home__title').text(title)
+    $('.highlight_home__desc').text(short)
+    $('.highlight_home__postedtime').text(postTime)
   })
 }
 
@@ -38,7 +39,7 @@ function sliderHighLightHome() {
     },
     watchSlidesVisibility: true,
     watchSlidesProgress: true
-  });
+  })
   new Swiper('.highlight_home__slider', {
     spaceBetween: 15,
     speed: 2500,
@@ -55,37 +56,23 @@ function sliderHighLightHome() {
     thumbs: {
       swiper: galleryThumbs
     },
-  });
+  })
 }
 
 function sliderJobHome() {
   new Swiper('.job_home__slider', {
     slidesPerView: 1,
-    // spaceBetween: 16,
     effect: 'fade',
     fadeEffect: {
       crossFade: true
     },
-    // effect: 'cube',
-    // grabCursor: true,
-    // cubeEffect: {
-    //   shadow: true,
-    //   slideShadows: true,
-    //   shadowOffset: 20,
-    //   shadowScale: 0.94,
-    // },
-
     loop: true,
-    // autoplay: {
-    //   delay: 4000,
-    //   disableOnInteraction: false,
-    // },
     speed: 1000,
     navigation: {
       nextEl: '.job_home__slider__navigation .next',
       prevEl: '.job_home__slider__navigation .prev'
     }
-  });
+  })
 }
 
 function sliderGalleryHome() {
@@ -108,10 +95,10 @@ function sliderGalleryHome() {
       el: '.gallery_home__slider .swiper-pagination',
       clickable: true
     },
-  });
+  })
 }
 
-function sliderParnerHome() {
+function sliderPartnerHome() {
   new Swiper('.partner_home__slider', {
     loop: true,
     speed: 3000,
@@ -129,5 +116,41 @@ function sliderParnerHome() {
         slidesPerView: 4
       }
     }
-  });
+  })
+}
+
+function sliderNewsHome() {
+  new Swiper('.home-news__main .swiper-container', {
+    slideToClickedSlide: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    spaceBetween: 16,
+    loop: true,
+    speed: 1500,
+    navigation: {
+      nextEl: '.home-news__main__nav-next',
+      prevEl: '.home-news__main__nav-prev'
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      768: {
+        speed: 3000
+      }
+    }
+  })
+  new Swiper('.home-news__sub .swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    autoHeight: true,
+    loop: true,
+    navigation: {
+      nextEl: '.home-news__sub__nav-next',
+      prevEl: '.home-news__sub__nav-prev'
+    },
+  })
 }
